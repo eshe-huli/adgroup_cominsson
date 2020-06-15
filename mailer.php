d938ca3dbbd3e8fe8dec04e0da9c916697e91c6b
Ben Ouattara <ainotekcorp@gmail.com>
2020-06-15 21:30:35 +0000

    design: preloader size, email address

--- mailer.php
+++ mailer.php
@@ -1,31 +1,31 @@
 <?php
 $errors = [];
-$myemail = "info@adgroupe.io";
 $name = $_POST[‘name’] || false;
+$myemail = "contact@adgroupe.io";
+$appName = "commingSoon Application";
 $message = $_POST[‘message’] || false;
 $email_address = $_POST[’email’] || false;
 $validEmailChecker = “/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i”;
 
 if(!$email)
-	$errors['email'] = "Email is required";
+	$errors['email'] = "l'addresse Email est requise";
  if(!$message)
-	$errors['message'] = "Message is required");
+	$errors['message'] = "le Message est requis");
 if (!preg_match($validEmailChecker, $email_address))
-	$errors['email_invalid'] = "Invalid email address");
+	$errors['email_invalid'] = "Addresse email invalide");
 
 if(!empty($errors)) {
 	$to = $myemail;
 	$subject = "Contact form submission: ${name}";
-	$body = "You have received a new message.\n
+	$body = "You have received a new message from ${appName}.\n
 	  Here are the details:\n
 	  Name: ${name} \n.
 	  Email: ${email_address}\n
 	  Message:\n
-	  $message";
+	  \t$message";
 	$headers = "From: ${myemail}\n";
 	$headers .= "Reply-To: ${email_address}";
 
 	mail($to,$subject,$body,$headers);
-
 }
-	header("Location: ./index.html");
+header("Location: ./index.html");
